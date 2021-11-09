2010e50d553950af7045dd78819a390c7ee97dd0
hampus <hampus.john.olausson@gmail.com>
2021-11-09 21:22:51 +0100

    implemented some simple tailwind stuling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div>
+        <div className="text-xl font-medium text-primary">ChitChat</div>
+        <p className="text-blue-450">You have a new message!</p>
+        <TailwindButton/>
+      </div>
     </div>
   );
+}
+
+function TailwindButton(){
+  return (
+    <button className="bg-blue-500
+    text-white 
+    font-medium
+    px-4 py-2
+    rounded-md
+    hover:bg-blue-600
+    ">Testing Button</button>
+  )
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+    extend: {
+      colors: {
+        primary: "#ff4800",
+        blue: {
+          450: '#5F99F7'
+        },
+      }
+    },
   },
   variants: {
     extend: {},
