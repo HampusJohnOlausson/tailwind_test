389d858d58671d38184382c47d9c49f9df8845c2
hampus <hampus.john.olausson@gmail.com>
2021-11-09 22:24:03 +0100

    Just playing around with tailwind css

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import './App.css';
 function App() {
   return (
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div>
+      <div className="container">
         <div className="text-xl font-medium text-primary">ChitChat</div>
         <p className="text-blue-450">You have a new message!</p>
-        <TailwindButton/>
+        <TailwindButton>
+          TailwindCSS
+        </TailwindButton>
+      </div>
+      <div className="flex-initial content-center justify-center mx-2">
+        <div className="bg-red-600  px-4 py-2 rounded border-4 mx-4 border-blue-450">hello</div>
+        <div>hello</div>
       </div>
     </div>
   );
 }
 
-function TailwindButton(){
+function TailwindButton(props){
   return (
     <button className="bg-blue-500
     text-white 
@@ -20,7 +26,7 @@ function TailwindButton(){
     px-4 py-2
     rounded-md
     hover:bg-blue-600
-    ">Testing Button</button>
+    ">{props.children}</button>
   )
 }
 
